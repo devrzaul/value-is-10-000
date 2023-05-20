680e258787a343760133505920ca214218d023b3
devrzaul <98711027+devrzaul@users.noreply.github.com>
2023-05-20 20:56:49 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// Create a loop where the initial value is 10,000. You need to rotate the loop 50 times by using the increment and decrement operators twice.
+
+
+for( let i = 10000; i <= 10050; i++){
+    console.log(`loop 50 times by using the increment : ${i} `);
+}
+
+for( let i = 10000; i >= 9950; i--){
+    console.log(`loop 50 times by using the decrement  : ${i} `);
+}
